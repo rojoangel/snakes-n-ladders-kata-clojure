0952581f4c1779df70eb0076f418defb7667e052
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-03-22 13:44:31 +0100

    Given the token is on square 1 when the token is moved 3 spaces And then it is moved 4 spaces Then the token is on square 8

--- test/snakes_n_ladders/core_test.clj
+++ test/snakes_n_ladders/core_test.clj
@@ -6,15 +6,21 @@
   {})
 
 (defn place-token [game]
-  1)
+  (assoc game :token 1))
 
 (defn move-token [game spaces]
-  4)
+  (update game :token #(+ % spaces)))
+
+(defn current-token [game]
+  (:token game))
 
 (deftest moving-your-token
   (testing "When the token is placed on the board then the token is on square 1"
     (let [game (new-game)]
-      (is (= 1 (place-token game)))))
+      (is (= 1 (current-token (place-token game))))))
   (testing "Given the token is on square 1 when the token is moved 3 spaces then the token is on square 4"
     (let [game (place-token (new-game))]
-      (is (= 4 (move-token game 3))))))
+      (is (= 4 (current-token (move-token game 3))))))
+  (testing "Given the token is on square 1 when the token is moved 3 spaces And then it is moved 4 spaces Then the token is on square 8"
+    (let [game (place-token (new-game))]
+      (is (= 8 (current-token (move-token (move-token game 3) 4)))))))
