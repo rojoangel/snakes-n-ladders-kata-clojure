00540f0ccf8e43f82bffdd15510782ab995c57d5
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-03-22 14:39:29 +0100

    use the proper generator dependency

--- src/snakes_n_ladders/dice.clj
+++ src/snakes_n_ladders/dice.clj
@@ -1,6 +1,6 @@
 (ns snakes-n-ladders.dice
   (:require [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]))
+            [clojure.test.check.generators :as gen]))
 
 (defn roll-spec [& {:keys [min max]}]
   (s/int-in min (inc max)))
